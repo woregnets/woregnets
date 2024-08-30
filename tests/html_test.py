e363ae3f8ee652ccbb659998a71a7efe7e29b398
Finn Petersen <fp7@posteo.net>
2024-08-30 09:51:09 +0200

    Add attribution to openstreetmap

--- tests/html_test.py
+++ tests/html_test.py
@@ -18,3 +18,5 @@ class HtmlTest(unittest.TestCase):
             self.assertEqual(3, len(bs.find(id="radar_images").find_all("img")))
             self.assertEqual("/build/radar_images/WN2408220915_000.png", bs.find(id="radar_images").find_all("img")[1]["src"])
             self.assertEqual("/build/radar_images/WN2408220920_000.png", bs.find(id="radar_images").find_all("img")[2]["src"])
+
+            self.assertTrue("Map data from <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a>" in str(bs.find(id="attribution")))
